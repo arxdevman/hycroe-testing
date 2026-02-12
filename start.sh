30c72792e2c3408c16a59036e449d8242117f1d4
arxdevman <fcgame4321@gmail.com>
2026-02-12 20:13:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,17 @@ set -e
 
 echo "Starting tmate session..."
 
-# Start tmate daemon with fixed socket
 tmate -S /tmp/tmate.sock new-session -d
-
-# Wait until tmate is ready
 tmate -S /tmp/tmate.sock wait tmate-ready
 
-# Print connection info (THIS IS WHAT YOU WANT)
+echo "TMATE SSH:"
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+
+echo "TMATE WEB:"
 tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
 
-# Keep container alive
-tail -f /dev/null
+# FORCE LOG FLUSH
+while true; do
+  echo "tmate alive: $(date)"
+  sleep 30
+done
